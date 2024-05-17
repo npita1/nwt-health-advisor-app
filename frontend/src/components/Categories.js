64e0630183a41c042e95d0584baf01712ba66922
npita1 <npita1@etf.unsa.ba>
2024-05-17 16:28:36 +0200

    Added categories div on HomePage

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -1,10 +1,106 @@
-import * as React from 'react'
+import React, { useState, useEffect } from 'react';
+import { getAllCategories } from '../services/forumService';
+import { Flex, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, useDisclosure } from '@chakra-ui/react';
+import '../styles/Categories.css'
 
 
 function Categories() {
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const fetchedCategories = await getAllCategories();
+        setCategories(fetchedCategories);
+      } catch (error) {
+        console.error('Error fetching categories:', error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    onOpen();
+  };
+
+  const firstRow = categories.slice(0,5);
+  const secondRow = categories.slice(5, 10);
+
+  const categoryIcons = {
+    Immunology: 'images/CategoryIcons/AllergyIcon.png',
+    Cardiology: 'images/CategoryIcons/CardiologyIcon.png',
+    Dermatology: 'images/CategoryIcons/DermatologyIcon.png',
+    Dentistry: 'images/CategoryIcons/DentistryIcon.png',
+    FamilyMedicine: 'images/CategoryIcons/FamilyMedicine.png',
+    Gastroenterology: '/images/CategoryIcons/GastroenterologyIcon.png',
+    Neurology: 'images/CategoryIcons/NeurologyIcon.png',
+    Ophthalmology: 'images/CategoryIcons/OphthalmologyIcon.png',
+    Orthopedics: 'images/CategoryIcons/OrthopedicsIcon.png',
+    Pediatrics: 'images/CategoryIcons/PediatricsIcon.png'
+  };
+  
+  
+
   return (
       <div className='tabelaKategorija'>
+          <Flex direction="column" className='divv'>
+            <h1 className='naslov'>Our consulting specialities</h1>
 
+            <Flex className='redovi'>
+            {
+              firstRow.map(category => (
+                <Flex direction="column" className='kategorija' onClick={() => handleCategoryClick(category)}>
+              <img
+              src={categoryIcons[category.name.replace(/\s/g, '')] || 'images/CategoryIcons/DefaultIcon.png'}
+              alt={category.name}
+              style={{ width: '70px', height: 'auto' }}
+            />
+                  <p className='nazivKategorije'>{category.name}</p>
+                </Flex>
+              ))
+            }
+            </Flex>
+
+            <Flex>
+            {
+              secondRow.map(category => (
+                <Flex direction="column" className='kategorija' onClick={() => handleCategoryClick(category)}>
+              <img
+              src={categoryIcons[category.name.replace(/\s/g, '')] || 'images/CategoryIcons/DefaultIcon.png'}
+              alt={category.name}
+              style={{ width: '70px', height: 'auto' }}
+            />
+                  <p className='nazivKategorije'>{category.name}</p>
+                </Flex>
+              ))
+            }
+            </Flex>
+
+          </Flex>
+
+
+          {selectedCategory && (
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>{selectedCategory.name}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <p>{selectedCategory.description}</p>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      )}
+      
       </div>
     
   )
---
+++ frontend/src/services/forumService.js
@@ -0,0 +1,21 @@
+
+const API_URL = 'http://localhost:8083';
+
+export async function getAllCategories() {
+    try {
+        const response = await fetch(`${API_URL}/forum/allCategories`, {
+          method: 'GET',
+        });
+    
+        if (response.ok) {
+          const data = await response.json();
+          console.log('Uspješno dohvaćeni podaci o kategorijama:');
+          return data; 
+        } else {
+          throw new Error('Došlo je do greške prilikom dohvaćanja podataka o kategorijama.');
+        }
+      } catch (error) {
+        console.error(`Došlo je do greške prilikom dohvaćanja podataka o kategorijama: ${error.message}`);
+        throw error;
+      }
+}
