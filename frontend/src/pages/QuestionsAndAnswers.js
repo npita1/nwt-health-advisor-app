644069b4e679cace614139d8d95fc7a6d0bc4ff0
npita1 <npita1@etf.unsa.ba>
2024-05-27 18:07:43 +0200

    Added ForumQuestion form on frontend

--- frontend/src/pages/QuestionsAndAnswers.js
+++ frontend/src/pages/QuestionsAndAnswers.js
@@ -2,12 +2,41 @@ import React, { useState, useEffect } from 'react';
 import '../styles/QuestionsAndAnswers.css';
 import '../styles/Categories.css';
 import Categories from '../components/CategoriesForum';
-import {Flex, Button, Box} from '@chakra-ui/react';
-import { getAllForumQuestions, getForumQuestionsByCategory} from '../services/forumService';
+import {
+    Flex,
+    Button,
+    Box,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    FormControl,
+    FormLabel,
+    Input,
+    Textarea,
+    Radio,
+    RadioGroup,
+    Stack,
+    Select
+} from '@chakra-ui/react';
+import {
+    getAllForumQuestions,
+    getForumQuestionsByCategory,
+    getAllCategories
+} from '../services/forumService';
 
 function QuestionsAndAnswers() {
     const [questions, setQuestions] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [questionTitle, setQuestionTitle] = useState('');
+    const [questionText, setQuestionText] = useState('');
+    const [questionCategory, setQuestionCategory] = useState('');
+    const [anonymity, setAnonymity] = useState(true); 
+    const [categories, setCategories] = useState([]);
 
     useEffect(() => {
         const fetchQuestions = async () => {
@@ -27,25 +56,51 @@ function QuestionsAndAnswers() {
     }, [selectedCategory]);
 
 
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const fetchedCategories = await getAllCategories();
+                setCategories(fetchedCategories);
+            } catch (error) {
+                console.error('Error fetching categories:', error);
+            }
+        };
+
+        fetchCategories();
+    }, []);
+
+
     const handleCategorySelect = (category) => {
         setSelectedCategory(category);
     };
 
+    const handleModalClose = () => {
+        setShowModal(false);
+        // Reset form fields
+        setQuestionTitle('');
+        setQuestionText('');
+        setQuestionCategory('');
+        setAnonymity(true); // Reset to true when modal is closed
+    };
 
-    const categoryIcons = {
-    Immunology: 'images/CategoryIcons/AllergyIcon.png',
-    Cardiology: 'images/CategoryIcons/CardiologyIcon.png',
-    Dermatology: 'images/CategoryIcons/DermatologyIcon.png',
-    Dentistry: 'images/CategoryIcons/DentistryIcon.png',
-    FamilyMedicine: 'images/CategoryIcons/FamilyMedicine.png',
-    Gastroenterology: '/images/CategoryIcons/GastroenterologyIcon.png',
-    Neurology: 'images/CategoryIcons/NeurologyIcon.png',
-    Ophthalmology: 'images/CategoryIcons/OphthalmologyIcon.png',
-    Orthopedics: 'images/CategoryIcons/OrthopedicsIcon.png',
-    Pediatrics: 'images/CategoryIcons/PediatricsIcon.png'
-  };
-    
-    return(
+    const handleQuestionSubmit = () => {
+        // Here you can handle form submission, e.g., submit data to backend
+        console.log('Question title:', questionTitle);
+        console.log('Question text:', questionText);
+        console.log('Question category:', questionCategory);
+        console.log('Anonymity:', anonymity);
+
+        setQuestionTitle('');
+        setQuestionText('');
+        setQuestionCategory('');
+        setAnonymity(true); // Reset to true after submission
+
+        // For demonstration, just close the modal
+        handleModalClose();
+    };
+
+
+    return (
         <div>
             <div className='categoryDiv'>
                 <Flex direction="column" className='categoryFlex'>
@@ -63,7 +118,7 @@ function QuestionsAndAnswers() {
 
             <div className='zaglavljePitanja'> 
                 <Flex justifyContent="space-between" alignItems="center" className='zaglavljeFlex'>
-                    <Button className='newQuestionDugme' colorScheme='#FF585F'>New Question</Button>
+                    <Button className='newQuestionDugme' colorScheme='#FF585F' onClick={() => setShowModal(true)}>New Question</Button>
                 </Flex>
             </div>
 
@@ -71,15 +126,15 @@ function QuestionsAndAnswers() {
                 <Flex direction="column" className='pitanjaFlex'>
                     {
                         questions.map(question => (
-                            <div className='pitanje'>
+                            <div className='pitanje' key={question.id}>
                                 <Flex direction="column">
                                     <Flex justifyContent="space-between" alignItems="center" className='naslovIIkona'>
                                         <Flex direction="column">
-                                        <p className='naslovPitanja'>{question.title}</p>
-                                        <p className='korisnikPitanja'>
-                                            By: {question.anonymity ? 'Anonymous' : `${question.user.firstName} ${question.user.lastName}`}
-                                        </p>
-                                        <p className='datumPitanja'>{question.date}</p>
+                                            <p className='naslovPitanja'>{question.title}</p>
+                                            <p className='korisnikPitanja'>
+                                                By: {question.anonymity ? 'Anonymous' : `${question.user.firstName} ${question.user.lastName}`}
+                                            </p>
+                                            <p className='datumPitanja'>{question.date}</p>
                                         </Flex>
                                         <img src="images/HomePage/doktor.png" alt="Slika" className='slikaDoktor' style={{ width: '80px', height: 'auto' }} />
                                     </Flex>
@@ -91,6 +146,47 @@ function QuestionsAndAnswers() {
                 </Flex>
             </div>
 
+            {/* Modal */}
+            <Modal isOpen={showModal} onClose={handleModalClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>New Question</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <FormControl id='questionTitle' isRequired>
+                            <FormLabel>Question Title</FormLabel>
+                            <Input placeholder='Enter question title' value={questionTitle} onChange={(e) => setQuestionTitle(e.target.value)} />
+                        </FormControl>
+                        <FormControl id='questionText' mt={4} isRequired>
+                            <FormLabel>Question Text</FormLabel>
+                            <Textarea placeholder='Enter question text' value={questionText} onChange={(e) => setQuestionText(e.target.value)} />
+                        </FormControl>
+                        <FormControl id='questionCategory' mt={4} isRequired>
+                            <FormLabel>Category</FormLabel>
+                            <Select placeholder='Select category' value={questionCategory} onChange={(e) => setQuestionCategory(e.target.value)}>
+                                {categories.map(category => (
+                                    <option key={category.id} value={category.id}>{category.name}</option>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <FormControl id='anonymity' mt={4}>
+                            <FormLabel>Anonymity</FormLabel>
+                            <RadioGroup value={anonymity.toString()} onChange={(value) => setAnonymity(value === 'true')}>
+                                <Stack spacing={5} direction='row'>
+                                    <Radio value='true'>Yes</Radio>
+                                    <Radio value='false'>No</Radio>
+                                </Stack>
+                            </RadioGroup>
+                        </FormControl>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button className='submitButton' colorScheme='#FF585F' mr={3} onClick={handleQuestionSubmit}>
+                            Submit
+                        </Button>
+                        <Button onClick={handleModalClose}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </div>
     );
 }
